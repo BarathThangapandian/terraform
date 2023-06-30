0392da5df0a5204a4cdec5d26e2f3ba81b5fd014
BarathThangapandian <55328713+BarathThangapandian@users.noreply.github.com>
2023-06-30 10:35:21 -0400

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "demo_var" {
+  name        = "demo_var"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["10.20.30.40/32"]
+
+ }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.20.30.40/32"]
+ }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["10.20.30.40/32"]
+ }
+}
