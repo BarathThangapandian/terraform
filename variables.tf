e0ad07f999a9301ec2a7d6ee056377082b75b9d6
BarathThangapandian <55328713+BarathThangapandian@users.noreply.github.com>
2023-07-01 10:33:30 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,3 +31,23 @@ resource "aws_security_group" "demo_var" {
     cidr_blocks      = [var.client_ip]
  }
 }
+
+
+
+
+2nd type : 
+
+root@ip-172-31-15-12:~/terraform# cat ec2.tf 
+resource "aws_instance" "barat_app" {
+  ami          = "ami-0b9ecf71fe947bbdd"
+  instance_type = var.instance
+}
+
+
+root@ip-172-31-15-12:~/terraform# cat var.tf 
+variable "instance" {
+  default = "t2.micro"
+}
+root@ip-172-31-15-12:~/terraform# 
+
+
