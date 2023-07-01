1a4f3000bcdd418dc16669b9138a7f3815156a38
BarathThangapandian <55328713+BarathThangapandian@users.noreply.github.com>
2023-07-01 10:02:03 -0400

    Update variables.tf
    added a variable declared code

--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
+#vaiable.tf
+
 variable "client_ip" {
  default = "10.60.60.60/32"
+}
+
+
+#sg.tf
+resource "aws_security_group" "demo_var" {
+  name        = "demo_var"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.client_ip]
+ }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.client_ip]
+ }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [var.client_ip]
+ }
 }
